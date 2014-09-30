5ee153de00efdd869b300b02e76ebc6502a2f32f
Michael Writhe <michael@writhem.com>
2014-09-29 23:30:29 -0600

    looks like records works. need further testing. maybe in WritheM Radio

--- js/options.js
+++ js/options.js
@@ -2,6 +2,8 @@
  *  The MIT License (MIT)
  *
  *  Copyright (c) 2014 Ulysse Manceron
+ * 
+ *  Edited By: Michael Writhe, 2014
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
